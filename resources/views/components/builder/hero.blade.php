ec181763416e2c47f06d8ff522e66cd90c06912f
TJ <tj@jiannius.com>
2021-12-18 23:13:54 +0800

    fixed content alignment

--- resources/views/components/builder/hero.blade.php
+++ resources/views/components/builder/hero.blade.php
@@ -5,12 +5,14 @@
     'bottomImg' => $image && $imagePosition === 'bottom' ? $image : null,
 ])
 
-<div class="relative min-h-[500px] md:min-h-[600px] 3xl:max-w-screen-xl 3xl:mx-auto {{ $attributes->get('bgcolor') }}">
-    @if ($attributes->has('slider'))
+@if ($attributes->has('slider'))
+    <div class="relative h-[500px] md:h-[600px] 3xl:max-w-screen-xl 3xl:mx-auto">
         <x-builder.slider :config="$attributes->get('slider')">
             {{ $slot }}
         </x-builder.slider>
-    @else
+    </div>
+@else
+    <div class="relative min-h-[500px] md:min-h-[600px] 3xl:max-w-screen-xl 3xl:mx-auto {{ $attributes->get('bgcolor') }}">
         @if ($bgImg)
             <div class="absolute inset-0 {{ $overlay ? 'bg-black/30' : '' }}">
                 <img src="{{ $bgImg }}" class="w-full h-full object-cover opacity-30" width="1200" height="700" alt="{{ $attributes->get('image-alt') }}">
@@ -19,66 +21,68 @@
             <div class="absolute inset-0 bg-black/30"></div>
         @endif
 
-        <div class="
-            max-w-screen-xl mx-auto h-full grid
-            {{ $leftImg || $rightImg ? 'gap-6 md:grid-cols-2' : '' }}
-        ">
-            @if ($leftImg || $rightImg)
-                <img 
-                    src="{{ $leftImg ?? $rightImg }}" 
-                    class="w-full h-full object-cover {{ $rightImg ? 'order-last' : '' }}" 
-                    width="1200" 
-                    height="700" 
-                    alt="{{ $attributes->get('image-alt') }}"
-                >
-            @endif
-
+        <div class="absolute inset-0">
             <div class="
-                max-w-screen-lg mx-auto relative h-full flex flex-col gap-8 py-12 px-6
-                {{ $valign === 'top' ? 'justify-start' : '' }}
-                {{ $valign === 'center' ? 'justify-center' : '' }}
-                {{ $valign === 'bottom' ? 'justify-end' : '' }}
-                {{ $align === 'center' ? 'text-center' : '' }} 
-                {{ $align === 'right' ? 'text-right' : '' }}
+                max-w-screen-xl mx-auto h-full grid
+                {{ $leftImg || $rightImg ? 'gap-6 md:grid-cols-2' : '' }}
             ">
-                @isset($title)
-                    <h1 class="text-3xl font-bold md:text-5xl {{ $overlay ? 'text-gray-200' : 'text-gray-900' }}">
-                        {{ $title }}
-                    </h1>
-                @endisset
-
-                @isset($subtitle)
-                    <h2 class="text-xl font-semibold md:text-2xl {{ $overlay ? 'text-gray-200' : 'text-gray-700' }}">
-                        {{ $subtitle }}
-                    </h2>
-                @endisset
-
-                @if($slot->isNotEmpty())
-                    <p class="text-lg font-medium md:text-lg {{ $overlay ? 'text-gray-200' : 'text-gray-600' }}">
-                        {{ $slot }}
-                    </p>
+                @if ($leftImg || $rightImg)
+                    <img 
+                        src="{{ $leftImg ?? $rightImg }}" 
+                        class="w-full h-full object-cover {{ $rightImg ? 'order-last' : '' }}" 
+                        width="1200" 
+                        height="700" 
+                        alt="{{ $attributes->get('image-alt') }}"
+                    >
                 @endif
-
-                @isset($cta)
-                    <div class="
-                        inline-flex items-center space-x-3
-                        {{ $align === 'center' ? 'justify-center' : '' }}
-                        {{ $align === 'right' ? 'justify-end' : '' }}
-                    ">
-                        {{ $cta }}
-                    </div>
-                @endisset
+    
+                <div class="
+                    max-w-screen-lg mx-auto relative h-full flex flex-col gap-8 py-12 px-6
+                    {{ $valign === 'top' ? 'justify-start' : '' }}
+                    {{ $valign === 'center' ? 'justify-center' : '' }}
+                    {{ $valign === 'bottom' ? 'justify-end' : '' }}
+                    {{ $align === 'center' ? 'text-center' : '' }} 
+                    {{ $align === 'right' ? 'text-right' : '' }}
+                ">
+                    @isset($title)
+                        <h1 class="text-3xl font-bold md:text-5xl {{ $overlay ? 'text-gray-200' : 'text-gray-900' }}">
+                            {{ $title }}
+                        </h1>
+                    @endisset
+    
+                    @isset($subtitle)
+                        <h2 class="text-xl font-semibold md:text-2xl {{ $overlay ? 'text-gray-200' : 'text-gray-700' }}">
+                            {{ $subtitle }}
+                        </h2>
+                    @endisset
+    
+                    @if($slot->isNotEmpty())
+                        <p class="text-lg font-medium md:text-lg {{ $overlay ? 'text-gray-200' : 'text-gray-600' }}">
+                            {{ $slot }}
+                        </p>
+                    @endif
+    
+                    @isset($cta)
+                        <div class="
+                            inline-flex items-center space-x-3
+                            {{ $align === 'center' ? 'justify-center' : '' }}
+                            {{ $align === 'right' ? 'justify-end' : '' }}
+                        ">
+                            {{ $cta }}
+                        </div>
+                    @endisset
+                </div>
+    
+                @if ($bottomImg)
+                    <img 
+                        src="{{ $bottomImg }}" 
+                        class="rounded-lg drop-shadow" 
+                        width="1200" 
+                        height="700" 
+                        alt="{{ $attributes->get('image-alt') }}"
+                    >
+                @endif
             </div>
-
-            @if ($bottomImg)
-                <img 
-                    src="{{ $bottomImg }}" 
-                    class="rounded-lg drop-shadow" 
-                    width="1200" 
-                    height="700" 
-                    alt="{{ $attributes->get('image-alt') }}"
-                >
-            @endif
         </div>
-    @endif
-</div>
+    </div>
+@endif
--- stubs/resources/views/livewire/web/home.blade.php
+++ stubs/resources/views/livewire/web/home.blade.php
@@ -1,14 +1,9 @@
 <main class="min-h-screen">
     <section>
-        <x-builder.hero slider>
-            <x-builder.slider slide image="https://images.unsplash.com/photo-1639736867865-4828c8ad1163?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"/>
-            <x-builder.slider slide image="https://images.unsplash.com/photo-1639738415512-1f122497ef9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"/>
-            <x-builder.slider slide image="https://images.unsplash.com/photo-1639616714095-07d638ee2594?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
-        </x-builder.hero>
-    </section>
-
-    <section>
-        <x-builder.hero>
+        <x-builder.hero
+            bgcolor="bg-indigo-800"
+            image="https://images.unsplash.com/photo-1639798124484-752f1c12bc4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+        >
             <x-slot name="title">Atomic Bomb</x-slot>
 
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, eveniet eaque, quibusdam blanditiis ex sapiente consequatur error non perspiciatis mollitia sunt? Consectetur amet possimus, totam facilis beatae fugiat voluptatem nihil?
@@ -18,6 +13,14 @@
                     Get Started
                 </x-button>
             </x-slot>
+        </x-builder.hero>
+    </section>
+
+    <section>
+        <x-builder.hero slider>
+            <x-builder.slider slide image="https://images.unsplash.com/photo-1639736867865-4828c8ad1163?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"/>
+            <x-builder.slider slide image="https://images.unsplash.com/photo-1639738415512-1f122497ef9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"/>
+            <x-builder.slider slide image="https://images.unsplash.com/photo-1639616714095-07d638ee2594?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
         </x-builder.hero>
     </section>
 
