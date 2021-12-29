4effe4172925cfbd6a20b8a4c39258b45c762f95
TJ <tj@jiannius.com>
2021-12-29 23:34:21 +0800

    remove navs config from app service provider

--- resources/views/components/admin-panel.blade.php
+++ resources/views/components/admin-panel.blade.php
@@ -1,188 +1,176 @@
-<x-notify.alert/>
-<x-notify.toast/>
-<x-notify.confirm/>
-<x-fullscreen-loader/>
+{{-- aside without subitems --}}
+@if ($attributes->has('aside') && $href)
+    <a href="{{ $href }}" class="pl-2" {{ $isActive ? 'data-active' : '' }}>
+        <div
+            class="
+                flex items-center gap-2 rounded-l-md px-4 py-2.5 text-white
+                {{ $isActive ? 'font-semibold bg-white/20 border-r-8 border-theme' : 'font-medium hover:bg-white/10' }}
+            "
+        >
+            @if ($attributes->get('icon'))
+                <x-icon name="{{ $attributes->get('icon') }}" type="{{ $attributes->get('icon-type') ?? 'regular' }}" size="20px"/>
+            @endif
 
-<div 
-    class="min-h-screen bg-gray-50 text-sm" 
-    x-data="{ toggled: false, animate: false }"
-    @if ($flash)
-        x-init="$dispatch('toast', { message: '{{ $flash->message }}', type: '{{ $flash->type }}' })"
-    @endif
->
+            <div class="truncate">{{ $slot }}</div>
+        </div>
+    </a>
+
+{{-- aside with subitems --}}
+@elseif ($attributes->has('aside'))
     <div
-        x-ref="void"
-        class="opacity-0"
-        :class="{
-            'transition-0 duration-100 ease-in-out': animate,
-            'fixed inset-0 z-20 bg-black opacity-80 lg:hidden': toggled,
-        }"
-        @click="toggled = false"
+        x-data="{ active: false, open: false }"
+        x-init="active = $refs.subitems.querySelectorAll('[data-active]').length > 0"
     >
+        <div x-on:click="open = true" class="cursor-pointer pl-2">
+            <div
+                x-bind:class="active && 'bg-white/10'"
+                class="flex items-center gap-2 rounded-l-md px-4 py-2.5 text-white"
+            >
+                @if ($attributes->get('icon'))
+                    <x-icon name="{{ $attributes->get('icon') }}" type="{{ $attributes->get('icon-type') ?? 'regular' }}" size="20px"/>
+                @endif
+
+                <div class="flex-grow truncate">{{ $slot }}</div>
+                <x-icon name="chevron-down"/>
+            </div>
+        </div>
+
+        <div
+            x-ref="subitems"
+            x-show="active || open"
+            x-on:click.away="open = false" 
+            class="bg-gray-900 text-gray-300 grid py-1.5 pl-4"
+        >
+            {{ $subitems }}
+        </div>
     </div>
 
-    <aside 
-        x-ref="aside"
-        class="fixed left-0 top-0 bottom-0 z-20 overflow-hidden bg-gray-800"
-        :class="{
-            'transition-0 duration-100 ease-in-out': animate,
-            'w-56 lg:w-0': toggled,
-            'w-0 lg:w-56': !toggled,
-        }"
-    >
-        <div class="flex flex-col">
-            <div class="py-3 px-5">
-                @isset($brand)
-                    {{ $brand }}
-                @else    
-                    <a href="{{ route('dashboard') }}" class="flex-shrink-0 flex items-center gap-2">
-                        <div class="w-8 h-8">
-                            <x-atom-logo small/>
-                        </div>
-                        <div class="text-white text-lg tracking-wider">
-                            <span class="font-bold">Atom</span><span class="font-light">CMS</span>
-                        </div>
-                    </a>
-                @endisset
-            </div>
+{{-- admin panel --}}
+@else
+    <x-notify.alert/>
+    <x-notify.toast/>
+    <x-notify.confirm/>
+    <x-fullscreen-loader/>
 
-            <div class="flex-shrink-0 text-xs text-gray-500 py-2 px-6">
-                NAVIGATION
-            </div>
-
-            <div class="flex-grow overflow-y-auto overflow-x-hidden">
-                @foreach ($navs->where('enabled', true) as $nav)
-                    @if ($navdropdown = $nav->dropdown ?? [])
-                        <div x-data="{ expand: false, active: {{ $nav->active ? 'true' : 'false' }} }">
-                            <a
-                                class="ml-2 py-2.5 px-4 rounded-l-md flex items-center text-white"
-                                :class="active && 'bg-gray-900 ml-0 pl-6 active'"
-                                href="{{ $nav->href ?? 'javascript: void(0)' }}"
-                                @click="expand = !expand"
-                            >
-                                <x-icon name="{{ $nav->icon }}" size="20px" class="mr-2 flex-shrink-0"/>
-                                <div class="flex-grow whitespace-nowrap">
-                                    <x-icon 
-                                        x-bind:name="expand || active ? 'chevron-down' : 'chevron-right'" 
-                                        size="20px" 
-                                        class="float-right"
-                                    />
-                                    {{ $nav->label }}
-                                </div>
-                            </a>
-    
-                            <div 
-                                class="bg-gray-900 text-gray-300 flex flex-col py-1.5"
-                                x-show="expand || active"
-                                @click.away="expand = false"
-                            >
-                                @foreach ($navdropdown as $item)
-                                    <a 
-                                        href="{{ $item->href }}"
-                                        class="py-1.5 pl-7 pr-2 ml-6 rounded-l-md whitespace-nowrap text-white"
-                                        :class="active && 'bg-gray-600 border-theme border-r-8 font-medium'"
-                                        x-data="{ active: {{ $item->active ? 'true' : 'false' }} }"
-                                    >
-                                        {{ $item->label }}
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
-                    @else
-                        <a 
-                            href="{{ $nav->href }}"
-                            class="ml-2 py-2.5 px-4 rounded-l-md flex items-center text-white" 
-                            :class="active && 'bg-gray-600 border-theme border-r-8 font-medium'"
-                            x-data="{ active: {{ $nav->active ? 'true' : 'false' }} }"
-                        >
-                            <x-icon name="{{ $nav->icon }}" size="20px" class="mr-2 flex-shrink-0"/>
-                            <div class="flex-grow">
-                                {{ $nav->label }}
-                            </div>
-                        </a>
-                    @endif
-                @endforeach
-            </div>
-        </div>
-    </aside>
-
-    <main
-        x-ref="container"
-        class="w-full"
-        :class="{
-            'transition-0 duration-100 ease-in-out': animate,
-            'lg:pl-0': toggled,
-            'lg:pl-56': !toggled,
-        }"
-    >
-        <div class="h-12 bg-white shadow flex items-center sticky top-0 z-10">
-            <a class="px-4 py-2 flex-shrink-0 text-gray-900" @click="toggled = !toggled; animate = true">
-                <x-icon name="menu"/>
-            </a>
-
-            <div class="flex-grow flex items-center">
-                @isset($navbarLeft)
-                    {{ $navbarLeft }}
-                @endisset
-            </div>
-
-            <div class="flex-shrink-0 flex items-center">
-                @isset($navbarRight)
-                    {{ $navbarRight }}
-                @endisset
-
-                <div x-data="{ open: false }" x-cloak class="relative px-4">
-                    <a class="inline-flex items-center justify-center font-medium text-gray-900 h-12" @click="open = true">
-                        <x-icon name="user-circle" class="mr-1.5" /> 
-                        <span class="text-sm hidden md:block">
-                            {{ Illuminate\Support\Str::limit(auth()->user()->name, 15) }}
-                        </span>
-                        <x-icon name="chevron-down" class="hidden md:block" />
-                    </a>
-
-                    <div 
-                        class="absolute right-4 bg-white rounded-md shadow-md border w-max mt-1 py-1.5 font-medium text-sm"
-                        x-show="open"
-                        x-transition
-                        @click.away="open = false"
-                    >
-                        @foreach ($dropdown as $item)
-                            <a href="{{ $item->href }}" class="py-2 px-5 flex items-center text-gray-900 hover:bg-gray-100">
-                                <x-icon name="{{ $item->icon }}" size="18px" class="mr-2" /> {{ $item->label }}
-                            </a>
-                        @endforeach
-
-                        <a href="{{ route('login', ['logout' => 1]) }}" class="py-2 px-5 flex items-center text-gray-900 hover:bg-gray-100">
-                            <x-icon name="log-out" size="18px" class="mr-2"/> Logout
-                        </a>
-
-                        <div class="py-2 px-5 flex items-center border-t hover:bg-gray-100">
-                            <x-icon name="rocket" size="18px" class="mr-2"/> Version {{ $version }}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        @if ($unverified)
-            <div class="py-3 px-4 bg-yellow-100 shadow" x-data>
-                <div class="md:flex md:items-center md:space-x-2">
-                    <x-icon name="error" class="flex-shrink-0 text-yellow-400"/>
-                    <div class="flex-grow font-medium text-yellow-600">
-                        We have sent a verification link to <span class="font-semibold">{{ request()->user()->email }}</span>, please click on the link to verify it.
-                    </div>
-                </div>
-                <a class="text-xs md:ml-8" x-on:click.prevent="$refs.form.submit()">
-                    Resend verification link
-                </a>
-                <form x-ref="form" method="POST" action="{{ route('verification.send') }}" class="hidden">
-                    @csrf
-                </form>
-            </div> 
+    <div 
+        class="min-h-screen bg-gray-50 text-sm" 
+        x-data="{ toggled: false, animate: false }"
+        @if ($flash)
+            x-init="$dispatch('toast', { message: '{{ $flash->message }}', type: '{{ $flash->type }}' })"
         @endif
-
-        <div class="px-5 pt-5 pb-20 md:px-8 md:pt-8">
-            {{ $slot }}
+    >
+        <div
+            x-ref="void"
+            class="opacity-0"
+            :class="{
+                'transition-0 duration-100 ease-in-out': animate,
+                'fixed inset-0 z-20 bg-black opacity-80 lg:hidden': toggled,
+            }"
+            @click="toggled = false"
+        >
         </div>
-    </main>
-</div>
+
+        <aside 
+            x-ref="aside"
+            class="fixed left-0 top-0 bottom-0 z-20 overflow-hidden bg-gray-800"
+            :class="{
+                'transition-0 duration-100 ease-in-out': animate,
+                'w-56 lg:w-0': toggled,
+                'w-0 lg:w-56': !toggled,
+            }"
+        >
+            <div class="flex flex-col h-full">
+                <div class="flex-shrink-0 py-3 px-5">
+                    @isset($brand)
+                        {{ $brand }}
+                    @else    
+                        <a href="{{ route('dashboard') }}" class="flex-shrink-0 flex items-center gap-2">
+                            <div class="w-8 h-8">
+                                <x-atom-logo small/>
+                            </div>
+                            <div class="text-white text-lg tracking-wider">
+                                <span class="font-bold">Atom</span><span class="font-light">CMS</span>
+                            </div>
+                        </a>
+                    @endisset
+                </div>
+
+                <div class="flex-grow overflow-y-auto overflow-x-hidden">
+                    <div class="text-xs text-gray-500 py-2 px-6">
+                        NAVIGATION
+                    </div>
+
+                    <div class="grid pb-10">
+                        {{ $aside }}
+                    </div>
+                </div>
+            </div>
+        </aside>
+
+        <main
+            x-ref="container"
+            class="w-full"
+            :class="{
+                'transition-0 duration-100 ease-in-out': animate,
+                'lg:pl-0': toggled,
+                'lg:pl-56': !toggled,
+            }"
+        >
+            <div class="h-12 bg-white shadow flex items-center gap-4 sticky top-0 z-10 px-4">
+                <a class="flex-shrink-0 text-gray-800 flex items-center justify-center" @click="toggled = !toggled; animate = true">
+                    <x-icon name="menu"/>
+                </a>
+
+                <div class="flex-grow flex items-center gap-4">
+                    @isset($links)
+                        {{ $links }}
+                    @endisset
+                </div>
+
+                <div class="flex-shrink-0 flex items-center gap-4">
+                    @isset($actions)
+                        {{ $actions }}
+                    @endisset
+
+                    <x-dropdown right>
+                        <x-slot name="trigger">
+                            <a class="flex items-center justify-center gap-2 font-medium text-gray-800" @click="open = true">
+                                <x-icon name="user-circle"/> 
+                                <span class="text-sm hidden md:block">{{ Str::limit(auth()->user()->name, 15) }}</span>
+                                <x-icon name="chevron-down" class="hidden md:block" />
+                            </a>
+                        </x-slot>
+
+                        @isset($dropdown)
+                            {{ $dropdown }}
+                        @endisset
+
+                        <x-dropdown item icon="log-out" href="{{ route('login', ['logout' => 1]) }}">Logout</x-dropdown>
+                        <x-dropdown item icon="rocket">Version {{ $version }}</x-dropdown>
+                    </x-dropdown>
+                </div>
+            </div>
+
+            @if ($unverified)
+                <div class="py-3 px-4 bg-yellow-100 shadow" x-data>
+                    <div class="md:flex md:items-center md:space-x-2">
+                        <x-icon name="error" class="flex-shrink-0 text-yellow-400"/>
+                        <div class="flex-grow font-medium text-yellow-600">
+                            We have sent a verification link to <span class="font-semibold">{{ request()->user()->email }}</span>, please click on the link to verify it.
+                        </div>
+                    </div>
+                    <a class="text-xs md:ml-8" x-on:click.prevent="$refs.form.submit()">
+                        Resend verification link
+                    </a>
+                    <form x-ref="form" method="POST" action="{{ route('verification.send') }}" class="hidden">
+                        @csrf
+                    </form>
+                </div> 
+            @endif
+
+            <div class="px-5 pt-5 pb-20 md:px-8 md:pt-8">
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+@endif
--- src/Components/AdminPanel.php
+++ src/Components/AdminPanel.php
@@ -2,16 +2,15 @@
 
 namespace Jiannius\Atom\Components;
 
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class AdminPanel extends Component
 {
-    public $navs;
-    public $bold;
-    public $light;
+    public $href;
     public $flash;
     public $version;
-    public $dropdown;
+    public $isActive;
     public $unverified;
 
     /**
@@ -19,27 +18,21 @@ class AdminPanel extends Component
      * 
      * @return void
      */
-    public function __construct($dropdown = [], $navs = [], $brand = 'NEW::APP')
-    {
-        $this->light = explode('::', $brand)[0];
-        $this->bold = explode('::', $brand)[1];
-        $this->dropdown = collect($dropdown)->map(fn($item) => (object)$item);
+    public function __construct(
+        $href = null,
+        $active = null
+    ) {
+        $this->href = $href;
         $this->flash = $this->getFlash();
         $this->version = $this->getVersion();
         $this->unverified = request()->user()->mustVerifyEmail && !request()->user()->hasVerifiedEmail();
 
-        $this->navs = collect($navs)->map(function ($nav) {
-            $nav = (object)$nav;
-
-            if (isset($nav->dropdown)) {
-                $nav->dropdown = collect($nav->dropdown)->map(fn($item) => (object)$item);
-                $nav->active = $nav->dropdown->where('active', true)->count() > 0;
-            }
-
-            if (!isset($nav->enabled)) $nav->enabled = true;
-
-            return $nav;
-        });
+        if (is_null($active)) {
+            $this->isActive = $href
+                ? Str::startsWith(url()->current(), $href)
+                : false;
+        }
+        else $this->isActive = $active;
     }
 
     /**
