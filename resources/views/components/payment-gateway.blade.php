995055283a75b95fdd6e238438d1a000267e17f2
TJ <tj@jiannius.com>
2022-03-27 10:25:02 +0800

    fixed button

--- resources/views/components/payment-gateway.blade.php
+++ resources/views/components/payment-gateway.blade.php
@@ -2,108 +2,110 @@
     <form 
         x-data="ozopay(@js($value), @js($attributes->get('callback')))"
         x-on:submit.prevent="submit()"
-        class="grid gap-4 p-5" 
+        class="grid gap-10 p-5" 
     >
         @csrf
 
-        <div class="grid gap-6 md:grid-cols-2">
+        <div class="grid gap-4">
+            <div class="grid gap-6 md:grid-cols-2">
+                <div>
+                    <x-input.field x-bind:class="{ error: errors.first_name }" required>
+                        <x-slot name="label">{{ __('First Name') }}</x-slot>
+                        <input type="text" x-model="value.first_name" class="form-input w-full">
+                        <div x-show="errors.first_name" class="text-sm text-red-500 font-medium mt-1">{{ __('First name is required.') }}</div>
+                    </x-input.field>
+                </div>
+    
+                <div>
+                    <x-input.field x-bind:class="{ error: errors.last_name }" required>
+                        <x-slot name="label">{{ __('Last Name') }}</x-slot>
+                        <input type="text" x-model="value.last_name" class="form-input w-full">
+                        <div x-show="errors.last_name" class="text-sm text-red-500 font-medium mt-1">{{ __('Last name is required.') }}</div>
+                    </x-input.field>
+                </div>
+            </div>
+    
+            <div class="grid gap-6 md:grid-cols-2">
+                <div>
+                    <x-input.field x-bind:class="{ error: errors.phone }" required>
+                        <x-slot name="label">{{ __('Phone') }}</x-slot>
+                        <input type="text" x-model="value.phone" class="form-input w-full">                   
+                        <div x-show="errors.phone" class="text-sm text-red-500 font-medium mt-1">{{ __('Phone is required.') }}</div>
+                    </x-input.field>
+                </div>
+    
+                <div>
+                    <x-input.field x-bind:class="{ error: errors.email }" required>
+                        <x-slot name="label">{{ __('Email') }}</x-slot>
+                        <input type="email" x-model="value.email" class="form-input w-full">    
+                        <div x-show="errors.email" class="text-sm text-red-500 font-medium mt-1">{{ __('Email is required.') }}</div>
+                    </x-input.field>
+                </div>
+            </div>
+    
             <div>
-                <x-input.field x-bind:class="{ error: errors.first_name }" required>
-                    <x-slot name="label">{{ __('First Name') }}</x-slot>
-                    <input type="text" x-model="value.first_name" class="form-input w-full">
-                    <div x-show="errors.first_name" class="text-sm text-red-500 font-medium mt-1">{{ __('First name is required.') }}</div>
+                <x-input.field x-bind:class="{ error: errors.address }" required>
+                    <x-slot name="label">{{ __('Billing Address') }}</x-slot>
+                    <input type="text" x-model="value.address" class="form-input w-full">
+                    <div x-show="errors.address" class="text-sm text-red-500 font-medium mt-1">{{ __('Address is required.') }}</div>
                 </x-input.field>
             </div>
-
-            <div>
-                <x-input.field x-bind:class="{ error: errors.last_name }" required>
-                    <x-slot name="label">{{ __('Last Name') }}</x-slot>
-                    <input type="text" x-model="value.last_name" class="form-input w-full">
-                    <div x-show="errors.last_name" class="text-sm text-red-500 font-medium mt-1">{{ __('Last name is required.') }}</div>
-                </x-input.field>
+    
+            <div class="grid gap-6 md:grid-cols-2">
+                <div>
+                    <x-input.field x-bind:class="{ error: errors.city }" required>
+                        <x-slot name="label">{{ __('City') }}</x-slot>
+                        <input type="text" x-model="value.city" class="form-input w-full">
+                        <div x-show="errors.city" class="text-sm text-red-500 font-medium mt-1">{{ __('City is required.') }}</div>
+                    </x-input.field>
+                </div>
+    
+                <div>
+                    <x-input.field x-bind:class="{ error: errors.postcode }" required>
+                        <x-slot name="label">{{ __('Postcode') }}</x-slot>
+                        <input type="text" x-model="value.postcode" class="form-input w-full">
+                        <div x-show="errors.postcode" class="text-sm text-red-500 font-medium mt-1">{{ __('Postcode is required.') }}</div>
+                    </x-input.field>
+                </div>
+            </div>
+    
+            <div class="grid gap-6 md:grid-cols-2">
+                <div>
+                    <x-input.field x-bind:class="{ error: errors.country }" required>
+                        <x-slot name="label">{{ __('Country') }}</x-slot>
+                        <select x-model="value.country" class="form-input w-full">
+                            <option value="">-- {{ __('Select Country') }} --</option>
+                            @foreach (metadata()->countries() as $country)
+                                <option value="{{ $country['iso_code'] }}">{{ $country['name'] }}</option>
+                            @endforeach
+                        </select>
+                        <div x-show="errors.country" class="text-sm text-red-500 font-medium mt-1">{{ __('Country is required.') }}</div>
+                    </x-input.field>
+                </div>
+    
+                <div x-show="value.country === 'MY'">
+                    <x-input.field x-bind:class="{ error: errors.state }" required>
+                        <x-slot name="label">{{ __('State') }}</x-slot>
+                        <select x-model="value.state" class="form-input w-full">
+                            <option value="">-- {{ __('Select State') }} --</option>
+                            @foreach (metadata()->states('MY') as $state)
+                                <option value="{{ $state['name'] }}">{{ $state['name'] }}</option>
+                            @endforeach
+                        </select>
+                        <div x-show="errors.state" class="text-sm text-red-500 font-medium mt-1">{{ __('State is required.') }}</div>
+                    </x-input.field>
+                </div>
+    
+                <div x-show="value.country !== 'MY'">
+                    <x-input.field required>
+                        <x-slot name="label">{{ __('State') }}</x-slot>
+                        <input type="text" x-model="value.state" class="form-input w-full">
+                    </x-input.field>
+                </div>
             </div>
         </div>
 
-        <div class="grid gap-6 md:grid-cols-2">
-            <div>
-                <x-input.field x-bind:class="{ error: errors.phone }" required>
-                    <x-slot name="label">{{ __('Phone') }}</x-slot>
-                    <input type="text" x-model="value.phone" class="form-input w-full">                   
-                    <div x-show="errors.phone" class="text-sm text-red-500 font-medium mt-1">{{ __('Phone is required.') }}</div>
-                </x-input.field>
-            </div>
-
-            <div>
-                <x-input.field x-bind:class="{ error: errors.email }" required>
-                    <x-slot name="label">{{ __('Email') }}</x-slot>
-                    <input type="email" x-model="value.email" class="form-input w-full">    
-                    <div x-show="errors.email" class="text-sm text-red-500 font-medium mt-1">{{ __('Email is required.') }}</div>
-                </x-input.field>
-            </div>
-        </div>
-
-        <div>
-            <x-input.field x-bind:class="{ error: errors.address }" required>
-                <x-slot name="label">{{ __('Billing Address') }}</x-slot>
-                <input type="text" x-model="value.address" class="form-input w-full">
-                <div x-show="errors.address" class="text-sm text-red-500 font-medium mt-1">{{ __('Address is required.') }}</div>
-            </x-input.field>
-        </div>
-
-        <div class="grid gap-6 md:grid-cols-2">
-            <div>
-                <x-input.field x-bind:class="{ error: errors.city }" required>
-                    <x-slot name="label">{{ __('City') }}</x-slot>
-                    <input type="text" x-model="value.city" class="form-input w-full">
-                    <div x-show="errors.city" class="text-sm text-red-500 font-medium mt-1">{{ __('City is required.') }}</div>
-                </x-input.field>
-            </div>
-
-            <div>
-                <x-input.field x-bind:class="{ error: errors.postcode }" required>
-                    <x-slot name="label">{{ __('Postcode') }}</x-slot>
-                    <input type="text" x-model="value.postcode" class="form-input w-full">
-                    <div x-show="errors.postcode" class="text-sm text-red-500 font-medium mt-1">{{ __('Postcode is required.') }}</div>
-                </x-input.field>
-            </div>
-        </div>
-
-        <div class="grid gap-6 md:grid-cols-2">
-            <div>
-                <x-input.field x-bind:class="{ error: errors.country }" required>
-                    <x-slot name="label">{{ __('Country') }}</x-slot>
-                    <select x-model="value.country" class="form-input w-full">
-                        <option value="">-- {{ __('Select Country') }} --</option>
-                        @foreach (metadata()->countries() as $country)
-                            <option value="{{ $country['iso_code'] }}">{{ $country['name'] }}</option>
-                        @endforeach
-                    </select>
-                    <div x-show="errors.country" class="text-sm text-red-500 font-medium mt-1">{{ __('Country is required.') }}</div>
-                </x-input.field>
-            </div>
-
-            <div x-show="value.country === 'MY'">
-                <x-input.field x-bind:class="{ error: errors.state }" required>
-                    <x-slot name="label">{{ __('State') }}</x-slot>
-                    <select x-model="value.state" class="form-input w-full">
-                        <option value="">-- {{ __('Select State') }} --</option>
-                        @foreach (metadata()->states('MY') as $state)
-                            <option value="{{ $state['name'] }}">{{ $state['name'] }}</option>
-                        @endforeach
-                    </select>
-                    <div x-show="errors.state" class="text-sm text-red-500 font-medium mt-1">{{ __('State is required.') }}</div>
-                </x-input.field>
-            </div>
-
-            <div x-show="value.country !== 'MY'">
-                <x-input.field required>
-                    <x-slot name="label">{{ __('State') }}</x-slot>
-                    <input type="text" x-model="value.state" class="form-input w-full">
-                </x-input.field>
-            </div>
-        </div>
-
-        <x-button color="green" size="md" class="mt-4" type="submit" x-bind:disabled="loading">
+        <x-button color="green" size="md" type="submit" block x-bind:disabled="loading">
             <span x-show="loading">{{ __('Loading') }}...</span>
             <span x-show="!loading">{{ __('Continue') }}</span>
         </x-button>
@@ -114,7 +116,7 @@
             Alpine.data('ozopay', (value, callback = null) => ({
                 value,
                 errors: {},
-                loading: false,
+                loading: true,
 
                 validated () {
                     this.errors = {}
