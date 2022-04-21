22e0963ff715824a0e7e614ad99eaa3277edee89
TJ <tj@jiannius.com>
2022-04-21 12:16:43 +0800

    removed account type check

--- resources/views/components/admin-panel.blade.php
+++ resources/views/components/admin-panel.blade.php
@@ -2,19 +2,32 @@
 @if ($attributes->has('aside'))
     @if ($attributes->has('can') && !auth()->user()->can($attributes->get('can')))
         {{-- No permission --}}
-    @elseif (
-        ($attributes->has('account') && $attributes->get('account') !== 'any' && !auth()->user()->isAccountType($attributes->get('account')))
-        || (!$attributes->has('account') && !auth()->user()->isAccountType(['root', 'system']))
-    )
-        {{-- No account type --}}
-    @else
-        @if ($href || ($route && Route::has($route)))
-            <a href="{{ $href ?: route($route, $params) }}" class="pl-2" {{ $isActive ? 'data-active' : '' }}>
+    @elseif ($href || ($route && Route::has($route)))
+        <a href="{{ $href ?: route($route, $params) }}" class="pl-2" {{ $isActive ? 'data-active' : '' }}>
+            <div
+                class="
+                    flex items-center gap-3 rounded-l-md px-4 py-2.5 text-white
+                    {{ $isActive ? 'font-semibold bg-white/20 border-r-8 border-theme' : 'font-medium hover:bg-white/10' }}
+                "
+            >
+                @if ($icon = $attributes->get('icon'))
+                    <x-icon name="{{ $icon }}" type="{{ $attributes->get('icon-type') ?? 'regular' }}" size="20px"/>
+                @elseif ($logo = $attributes->get('logo'))
+                    <x-logo :src="$logo" class="brightness-0 invert" style="width: 20px; height: 20px;"/>
+                @endif
+
+                <div class="truncate">{{ $slot }}</div>
+            </div>
+        </a>
+    @elseif (!$href && !$route)
+        <div
+            x-data="{ active: false, open: false }"
+            x-init="active = $refs.subitems.querySelectorAll('[data-active]').length > 0"
+        >
+            <div x-on:click="open = true" class="cursor-pointer pl-2">
                 <div
-                    class="
-                        flex items-center gap-3 rounded-l-md px-4 py-2.5 text-white
-                        {{ $isActive ? 'font-semibold bg-white/20 border-r-8 border-theme' : 'font-medium hover:bg-white/10' }}
-                    "
+                    x-bind:class="active && 'bg-white/10'"
+                    class="flex items-center gap-3 rounded-l-md px-4 py-2.5 text-white"
                 >
                     @if ($icon = $attributes->get('icon'))
                         <x-icon name="{{ $icon }}" type="{{ $attributes->get('icon-type') ?? 'regular' }}" size="20px"/>
@@ -22,42 +35,22 @@
                         <x-logo :src="$logo" class="brightness-0 invert" style="width: 20px; height: 20px;"/>
                     @endif
 
-                    <div class="truncate">{{ $slot }}</div>
+                    <div class="flex-grow truncate">{{ $slot }}</div>
+                    <x-icon name="chevron-down"/>
                 </div>
-            </a>
-        @elseif (!$href && !$route)
-            <div
-                x-data="{ active: false, open: false }"
-                x-init="active = $refs.subitems.querySelectorAll('[data-active]').length > 0"
-            >
-                <div x-on:click="open = true" class="cursor-pointer pl-2">
-                    <div
-                        x-bind:class="active && 'bg-white/10'"
-                        class="flex items-center gap-3 rounded-l-md px-4 py-2.5 text-white"
-                    >
-                        @if ($icon = $attributes->get('icon'))
-                            <x-icon name="{{ $icon }}" type="{{ $attributes->get('icon-type') ?? 'regular' }}" size="20px"/>
-                        @elseif ($logo = $attributes->get('logo'))
-                            <x-logo :src="$logo" class="brightness-0 invert" style="width: 20px; height: 20px;"/>
-                        @endif
-
-                        <div class="flex-grow truncate">{{ $slot }}</div>
-                        <x-icon name="chevron-down"/>
-                    </div>
-                </div>
-
-                @if (isset($subitems) && $subitems->isNotEmpty())
-                    <div
-                        x-ref="subitems"
-                        x-show="active || open"
-                        x-on:click.away="open = false" 
-                        class="bg-gray-900 text-gray-300 grid py-1.5 pl-4"
-                    >
-                        {{ $subitems }}
-                    </div>
-                @endif
             </div>
-        @endif
+
+            @if (isset($subitems) && $subitems->isNotEmpty())
+                <div
+                    x-ref="subitems"
+                    x-show="active || open"
+                    x-on:click.away="open = false" 
+                    class="bg-gray-900 text-gray-300 grid py-1.5 pl-4"
+                >
+                    {{ $subitems }}
+                </div>
+            @endif
+        </div>
     @endif
 
 {{-- admin panel --}}
