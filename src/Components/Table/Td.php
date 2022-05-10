d19c49029cfb98a8ff990b446613b0621cc217f8
TJ <tj@jiannius.com>
2022-05-10 08:15:57 +0800

    added date and datetime

--- src/Components/Table/Td.php
+++ src/Components/Table/Td.php
@@ -13,10 +13,14 @@ class Td extends Component
      */
     public function __construct(
         $label = null,
+        $date = null,
+        $datetime = null,
         $percentage = null
     ) {
         if ($label) $this->label = $label;
         else if ($percentage) $this->label = number_format($percentage, 2).'%';
+        else if ($date) $this->label = format_date($date);
+        else if ($datetime) $this->label = format_date($datetime, 'datetime');
     }
 
     /**
