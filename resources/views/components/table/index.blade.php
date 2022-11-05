b46ecf06c6a4b7af9c13e9ffc0a50848e100c0ea
TJ <tj@jiannius.com>
2022-11-05 18:39:12 +0800

    fixed overflow

--- resources/views/components/table/index.blade.php
+++ resources/views/components/table/index.blade.php
@@ -12,7 +12,7 @@
             return rows <= 0
         },
     }"
-    class="relative flex flex-col divide-y bg-white border shadow rounded-lg overflow-hidden"
+    class="relative flex flex-col divide-y bg-white border shadow rounded-lg"
 >
     @isset($header) {{ $header }} @endif
 
@@ -22,7 +22,7 @@
         @endif
     </div>
 
-    <div x-show="!isEmpty" class="w-full overflow-auto max-h-screen">
+    <div x-show="!isEmpty" class="w-full overflow-auto max-h-screen rounded-b-lg">
         <table class="w-max divide-y divide-gray-200 md:w-full md:max-w-full">
             @isset($thead)
                 <thead>
