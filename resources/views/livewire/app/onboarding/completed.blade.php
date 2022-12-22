2fd3ec3697f19a38151c096bae8781a905925b98
TJ <tj@jiannius.com>
2022-12-22 12:35:11 +0800

    typo

--- resources/views/livewire/app/onboarding/completed.blade.php
+++ resources/views/livewire/app/onboarding/completed.blade.php
@@ -1,7 +1,7 @@
 <div class="grid gap-10">
     <div class="grid gap-1">
         <div class="text-3xl font-bold">
-            {{ __('You account setup is completed. Thank you for signing up with us') }}
+            {{ __('You account setup is completed. Thank you for signing up with us.') }}
         </div>
     
         <div class="text-gray-500 text-lg font-medium">
