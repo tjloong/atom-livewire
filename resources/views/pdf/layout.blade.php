6fed540ef53ed8125daf61170f2bd36c63802937
TJ <tj@jiannius.com>
2023-03-28 13:09:39 +0800

    pdf styles

--- resources/views/pdf/layout.blade.php
+++ resources/views/pdf/layout.blade.php
@@ -2,6 +2,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>{{ $filename }}</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
     @vite('resources/css/pdf.css')
 </head>
 <body>
