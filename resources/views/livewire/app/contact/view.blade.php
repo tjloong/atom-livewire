3b8c00ddd1a3bc6b26d4801e44fe9c76382d0109
TJ <tj@jiannius.com>
2023-04-05 16:36:18 +0800

    added label value for items

--- resources/views/livewire/app/contact/view.blade.php
+++ resources/views/livewire/app/contact/view.blade.php
@@ -36,15 +36,27 @@
             <x-box>
                 <div class="grid divide-y">
                     @foreach (collect($this->fields)->filter() as $field)
-                        @if (data_get($field, 'type') === 'items')
+                        @if (
+                            (data_get($field, 'type') === 'items' && ($items = data_get($field, 'value')))
+                            || ($items = data_get($field, 'items'))
+                        )
                             <div class="py-2 px-4">
                                 <x-form.items :label="data_get($field, 'label')">
                                     <div class="grid divide-y">
-                                        @foreach (data_get($field, 'value') as $item)
-                                            <div class="text-sm py-2 px-4 flex gap-2">
+                                        @foreach ($items as $item)
+                                            <div class="text-sm py-2 px-4">
                                                 @if (is_string($item)) {!! $item !!}
+                                                @elseif ($label = data_get($item, 'label'))
+                                                    <div class="flex items-center justify-between gap-2">
+                                                        <div class="text-gray-500 font-medium">{{ $label }}</div>
+                                                        <div>{!! data_get($item, 'value') !!}</div>
+                                                    </div>
+                                                @elseif ($icon = data_get($item, 'icon')) 
+                                                    <div class="flex gap-2">
+                                                        <x-icon :name="$icon" size="12" class="shrink-0 text-gray-400 mt-1"/>
+                                                        <div>{!! data_get($item, 'value') !!}</div>
+                                                    </div>
                                                 @else
-                                                    @if ($icon = data_get($item, 'icon')) <x-icon :name="$icon" size="12" class="shrink-0 text-gray-400 mt-1"/> @endif
                                                     {!! data_get($item, 'value') !!}
                                                 @endif
                                             </div>
