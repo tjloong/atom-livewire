856e42789bea8aa7df46702cb4ed33a4a721a398
TJ <tj@jiannius.com>
2023-06-24 19:42:46 +0800

    added div wrapper

--- resources/views/components/alert/errors.blade.php
+++ resources/views/components/alert/errors.blade.php
@@ -1,18 +1,20 @@
 @if ($errors->any())
-    <div class="p-4 rounded-md bg-red-100 border-red-300">
-        <div class="grid gap-1">
-            @foreach ($errors->all() as $error)
-                <div class="flex items-center gap-2">
-                    <x-icon 
-                        name="circle-xmark" 
-                        class="text-red-400 shrink-0" 
-                        size="20"
-                    />
-                    <div class="text-red-600 font-medium">
-                        {{ __($error) }}
+    <div {{ $attributes }}>
+        <div class="p-4 rounded-md bg-red-100 border-red-300">
+            <div class="grid gap-1">
+                @foreach ($errors->all() as $error)
+                    <div class="flex items-center gap-2">
+                        <x-icon 
+                            name="circle-xmark" 
+                            class="text-red-400 shrink-0" 
+                            size="20"
+                        />
+                        <div class="text-red-600 font-medium">
+                            {{ __($error) }}
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 @endif
