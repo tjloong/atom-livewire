6b393fd01c9eacc57f119590aab10d6c0ed5f7ab
TJ <tj@jiannius.com>
2023-06-26 18:52:51 +0800

    site settings structure

--- src/Models/SiteSetting.php
+++ src/Models/SiteSetting.php
@@ -133,14 +133,12 @@ class SiteSetting extends Model
         $inserts = collect();
 
         foreach ($defaults as $name => $value) {
-            if ($name === 'analytics') {
-                $existingData = [
-                    'fathom_id' => data_get($settings, 'fathom_id'),
-                    'ga_id' => data_get($settings, 'ga_id'),
-                    'gtm_id' => data_get($settings, 'gtm_id'),
-                    'fbp_id' => data_get($settings, 'fbpixel_id'),
-                ];
-            }
+            if ($name === 'popup_content') $existingData = data_get($settings, 'popup.content');
+            else if ($name === 'popup_delay') $existingData = data_get($settings, 'popup.delay');
+            else if ($name === 'facebook_pixel_id') $existingData = data_get($settings, 'analytics.fbp_id') ?? data_get($settings, 'fbpixel_id');
+            else if ($name === 'fathom_analytics_id') $existingData = data_get($settings, 'analytics.fathom_id') ?? data_get($settings, 'fathom_id');
+            else if ($name === 'google_analytics_id') $existingData = data_get($settings, 'analytics.ga_id') ?? data_get($settings, 'ga_id');
+            else if ($name === 'google_tag_manager_id') $existingData = data_get($settings, 'analytics.gtm_id') ?? data_get($settings, 'gtm_id');
             else $existingData = data_get($settings, $name);
 
             $value = $existingData ?? $value;
