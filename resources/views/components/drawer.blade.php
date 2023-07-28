596fe56046826c11b5ef64ef6c431ec1e9ed60f5
TJ <tj@jiannius.com>
2023-07-28 22:09:16 +0800

    added size

--- resources/views/components/drawer.blade.php
+++ resources/views/components/drawer.blade.php
@@ -1,5 +1,6 @@
 @props([
     'id' => component_id($attributes, 'drawer'),
+    'size' => $attributes->get('size', 'md'),
 ])
 
 <div
@@ -28,7 +29,11 @@
     @else <x-modal.overlay x-on:click.stop="$dispatch('close')"/>
     @endif
 
-    <div class="absolute top-0 bottom-0 right-0 left-0 py-1 pl-2 md:left-auto md:w-1/2 lg:w-4/12">
+    <div class="absolute top-0 bottom-0 right-0 left-0 py-1 pl-2 md:left-auto {{ [
+        'sm' => 'md:w-1/2 lg:w-2/12',
+        'md' => 'md:w-1/2 lg:w-4/12',
+        'lg' => 'md:w-2/3 lg:w-1/2',
+    ][$size] }}">
         <div {{ $attributes->class([
             'shadow-lg rounded-l-lg border flex flex-col w-full h-full',
             $attributes->get('class', 'bg-white'),
