749b4f1ec39060e2bcff742c6fdc9537e126a3a5
TJ <tj@jiannius.com>
2023-10-11 22:36:48 +0800

    heading

--- resources/views/components/sidenav/index.blade.php
+++ resources/views/components/sidenav/index.blade.php
@@ -1,33 +1,31 @@
-@props([
-    'model' => $attributes->wire('model')->value(),
-])
-
 <div 
-    x-data="{ 
+    x-data="{
+        value: @entangle($attributes->wire('model')),
         show: false,
         label: null,
-        @if ($model) value: @entangle($model),
-        @else value: @js($attributes->get('value')),
-        @endif
     }"
     x-on:input="label = $event.detail.label"
-    class="sidenav flex flex-col gap-4">
+    class="sidenav flex flex-col">
     {{-- mobile --}}
     <div x-on:click="show = !show" class="flex items-center gap-3 cursor-pointer md:cursor-auto md:hidden">
         <x-icon name="bars" class="text-gray-500"/>
 
         @isset($heading) {{ $heading }}
-        @elseif($attributes->get('title')) <div class="text-xl font-bold">{{ __($attributes->get('title')) }}</div>
+        @elseif($attributes->get('heading')) <div class="text-xl font-bold">{{ __($attributes->get('heading')) }}</div>
         @else <div x-text="label" class="font-semibold"></div>
         @endisset
     </div>
 
     {{-- desktop --}}
-    <div class="hidden md:block">
-        @isset($heading) {{ $heading }}
-        @elseif($attributes->get('title')) <div class="text-xl font-bold">{{ __($attributes->get('title')) }}</div>
-        @endisset
-    </div>
+    @isset($heading) 
+        <div class="hidden mb-4 md:block">
+            {{ $heading }}
+        </div>
+    @elseif($attributes->get('heading')) 
+        <div class="hidden mb-4 md:block">
+            <div class="text-xl font-bold">{{ __($attributes->get('heading')) }}</div>
+        </div>
+    @endisset
 
     <div x-show="screensize('sm') ? show : true" x-collapse class="flex flex-col">
         {{ $slot }}
