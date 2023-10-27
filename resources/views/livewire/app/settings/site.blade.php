075c7f7468c15a166d822a495c1f41280d794dbc
TJ <tj@jiannius.com>
2023-10-27 16:15:55 +0800

    recaptcha

--- resources/views/livewire/app/settings/site.blade.php
+++ resources/views/livewire/app/settings/site.blade.php
@@ -10,77 +10,85 @@
                 wire:model.defer="settings.site_description"/>
         </x-form.group>
 
-        <x-form.group cols="2" heading="Contact Information">
+        <x-form.group cols="2" heading="settings.heading.site-contact-info">
             <x-form.text label="settings.label.contact-name"
-                wire:model.defer="settings.site_contact_name"/>
+                wire:model.defer="settings.contact_name"/>
 
             <x-form.text label="settings.label.contact-phone"
-                wire:model.defer="settings.site_contact_phone"/>
+                wire:model.defer="settings.contact_phone"/>
 
             <x-form.text label="settings.label.contact-email"
-                wire:model.defer="settings.site_contact_email"/>
+                wire:model.defer="settings.contact_email"/>
 
             <x-form.text label="settings.label.contact-address"
-                wire:model.defer="settings.site_contact_address"/>
+                wire:model.defer="settings.contact_address"/>
 
             <x-form.text label="settings.label.contact-map"
-                wire:model.defer="settings.site_contact_map"/>
+                wire:model.defer="settings.contact_map"/>
         </x-form.group>
 
-        <x-form.group cols="2" heading="Whatsapp">
+        <x-form.group cols="2" heading="settings.heading.site-whatsapp">
             <x-form.text label="settings.label.whatsapp-number"
-                wire:model.defer="settings.site_whatsapp_number"/>
+                wire:model.defer="settings.whatsapp_number"/>
 
             <x-form.text label="settings.label.whatsapp-text"
-                wire:model.defer="settings.site_whatsapp_text"/>
+                wire:model.defer="settings.whatsapp_text"/>
 
             <x-form.checkbox label="settings.label.whatsapp-bubble"
-                wire:model="settings.site_whatsapp_bubble"/>
+                wire:model="settings.whatsapp_bubble"/>
+        </x-form.group>
+
+        <x-form.group cols="2" heading="settings.heading.site-social-media-page">
+            <x-form.text label="settings.label.facebook-url"
+                wire:model.defer="settings.facebook_url"/>
+
+            <x-form.text label="settings.label.instagram-url"
+                wire:model.defer="settings.instagram_url"/>
+
+            <x-form.text label="settings.label.twitter-url"
+                wire:model.defer="settings.twitter_url"/>
+
+            <x-form.text label="settings.label.linkedin-url"
+                wire:model.defer="settings.linkedin_url"/>
+
+            <x-form.text label="settings.label.youtube-url"
+                wire:model.defer="settings.youtube_url"/>
+
+            <x-form.text label="settings.label.spotify-url"
+                wire:model.defer="settings.spotify_url"/>
+
+            <x-form.text label="settings.label.tiktok-url"
+                wire:model.defer="settings.tiktok_url"/>
         </x-form.group>
 
         <x-form.group cols="2" heading="SEO">
             <x-form.text label="settings.label.meta-title"
-                wire:model.defer="settings.site_meta_title"/>
+                wire:model.defer="settings.meta_title"/>
 
             <x-form.text label="settings.label.meta-description"
-                wire:model.defer="settings.site_meta_description"/>
+                wire:model.defer="settings.meta_description"/>
 
             <x-form.text label="settings.label.meta-image"
-                wire:model.defer="settings.site_meta_image"/>
+                wire:model.defer="settings.meta_image"/>
         </x-form.group>
 
-        <x-form.group cols="2" heading="Analytics">
+        <x-form.group cols="2" heading="settings.heading.site-analytic">
             <x-form.text label="settings.label.ga-id"
-                wire:model.defer="settings.site_ga_id"/>
+                wire:model.defer="settings.ga_id"/>
 
             <x-form.text label="settings.label.gtm-id"
-                wire:model.defer="settings.site_gtm_id"/>
+                wire:model.defer="settings.gtm_id"/>
 
             <x-form.text label="settings.label.fbpixel-id"
-                wire:model.defer="settings.site_fbpixel_id"/>
+                wire:model.defer="settings.fbpixel_id"/>
         </x-form.group>
 
-        <x-form.group cols="2" heading="Social Media Pages">
-            <x-form.text label="settings.label.facebook-url"
-                wire:model.defer="settings.site_facebook_url"/>
-
-            <x-form.text label="settings.label.instagram-url"
-                wire:model.defer="settings.site_instagram_url"/>
-
-            <x-form.text label="settings.label.twitter-url"
-                wire:model.defer="settings.site_twitter_url"/>
-
-            <x-form.text label="settings.label.linkedin-url"
-                wire:model.defer="settings.site_linkedin_url"/>
-
-            <x-form.text label="settings.label.youtube-url"
-                wire:model.defer="settings.site_youtube_url"/>
-
-            <x-form.text label="settings.label.spotify-url"
-                wire:model.defer="settings.site_spotify_url"/>
-
-            <x-form.text label="settings.label.tiktok-url"
-                wire:model.defer="settings.site_tiktok_url"/>
+        <x-form.group cols="2" heading="settings.heading.site-recaptcha">
+            <x-form.text label="settings.label.site-key"
+                wire:model.defer="settings.recaptcha_site_key"/>
+                
+            <x-form.text label="settings.label.secret-key"
+                wire:model.defer="settings.recaptcha_secret_key"/>
         </x-form.group>
     </x-form>
 </div>
--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -32,7 +32,7 @@ class Setting extends Model
             return $this->get()
                 ->mapWithKeys(fn($val) => [
                     $val->name => in_array(data_get($val, 'name'), [
-                        'site_whatsapp_bubble',
+                        'whatsapp_bubble',
                         'revenue_monster_is_sandbox',
                     ]) ? json_decode($val->value, true) : $val->value,
                 ])
@@ -50,28 +50,27 @@ class Setting extends Model
         foreach ($defaults as $name => $value) {
             if ($name === 'popup_content') $existingData = data_get($settings, 'popup.content');
             else if ($name === 'popup_delay') $existingData = data_get($settings, 'popup.delay');
-            else if ($name === 'site_fbpixel_id') $existingData = data_get($settings, 'facebook_pixel_id') ?? data_get($settings, 'analytics.fbp_id') ?? data_get($settings, 'fbpixel_id');
-            else if ($name === 'site_ga_id') $existingData = data_get($settings, 'google_analytics_id') ?? data_get($settings, 'analytics.ga_id') ?? data_get($settings, 'ga_id');
-            else if ($name === 'site_gtm_id') $existingData = data_get($settings, 'google_tag_manager_id') ?? data_get($settings, 'analytics.gtm_id') ?? data_get($settings, 'gtm_id');
+            else if ($name === 'fbpixel_id') $existingData = data_get($settings, 'facebook_pixel_id') ?? data_get($settings, 'analytics.fbp_id') ?? data_get($settings, 'fbpixel_id');
+            else if ($name === 'ga_id') $existingData = data_get($settings, 'google_analytics_id') ?? data_get($settings, 'analytics.ga_id') ?? data_get($settings, 'ga_id');
+            else if ($name === 'gtm_id') $existingData = data_get($settings, 'google_tag_manager_id') ?? data_get($settings, 'analytics.gtm_id') ?? data_get($settings, 'gtm_id');
             else if ($name === 'site_name') $existingData = data_get($settings, 'company');
-            else if ($name === 'site_contact_phone') $existingData = data_get($settings, 'phone');
-            else if ($name === 'site_contact_email') $existingData = data_get($settings, 'email');
-            else if ($name === 'site_contact_address') $existingData = data_get($settings, 'address');
-            else if ($name === 'site_contact_map') $existingData = data_get($settings, 'gmap_url');
             else if ($name === 'site_description') $existingData = data_get($settings, 'briefs');
-            else if ($name === 'site_facebook_url') $existingData = data_get($settings, 'facebook');
-            else if ($name === 'site_instagram_url') $existingData = data_get($settings, 'instagram');
-            else if ($name === 'site_twitter_url') $existingData = data_get($settings, 'twitter');
-            else if ($name === 'site_linkedin_url') $existingData = data_get($settings, 'linkedin');
-            else if ($name === 'site_youtube_url') $existingData = data_get($settings, 'youtube');
-            else if ($name === 'site_spotify_url') $existingData = data_get($settings, 'spotify');
-            else if ($name === 'site_tiktok_url') $existingData = data_get($settings, 'tiktok');
-            else if ($name === 'site_meta_title') $existingData = data_get($settings, 'seo_title');
-            else if ($name === 'site_meta_description') $existingData = data_get($settings, 'seo_description');
-            else if ($name === 'site_meta_image') $existingData = data_get($settings, 'seo_image');
+            else if ($name === 'contact_name') $existingData = data_get($settings, 'company');
+            else if ($name === 'contact_phone') $existingData = data_get($settings, 'phone');
+            else if ($name === 'contact_email') $existingData = data_get($settings, 'email');
+            else if ($name === 'contact_address') $existingData = data_get($settings, 'address');
+            else if ($name === 'contact_map') $existingData = data_get($settings, 'gmap_url');
+            else if ($name === 'facebook_url') $existingData = data_get($settings, 'facebook');
+            else if ($name === 'instagram_url') $existingData = data_get($settings, 'instagram');
+            else if ($name === 'twitter_url') $existingData = data_get($settings, 'twitter');
+            else if ($name === 'linkedin_url') $existingData = data_get($settings, 'linkedin');
+            else if ($name === 'youtube_url') $existingData = data_get($settings, 'youtube');
+            else if ($name === 'spotify_url') $existingData = data_get($settings, 'spotify');
+            else if ($name === 'tiktok_url') $existingData = data_get($settings, 'tiktok');
+            else if ($name === 'meta_title') $existingData = data_get($settings, 'seo_title');
+            else if ($name === 'meta_description') $existingData = data_get($settings, 'seo_description');
+            else if ($name === 'meta_image') $existingData = data_get($settings, 'seo_image');
             else if ($name === 'site_whatsapp_number') $existingData = data_get($settings, 'whatsapp');
-            else if ($name === 'site_whatsapp_bubble') $existingData = data_get($settings, 'whatsapp_bubble"');
-            else if ($name === 'site_whatsapp_text') $existingData = data_get($settings, 'whatsapp_text');
             else $existingData = data_get($settings, $name);
 
             $value = empty($existingData) ? $value : $existingData;
