c48cda85414eb3ac0712a572f08b914f721754a2
TJ <tj@jiannius.com>
2023-11-25 22:21:23 +0800

    __ to tr()

--- resources/views/components/dropdown/item.blade.php
+++ resources/views/components/dropdown/item.blade.php
@@ -13,13 +13,13 @@
         'py-2 px-4 flex items-center gap-3 text-gray-800 font-normal hover:bg-slate-100'
     ])->except(['href', 'icon', 'label']) }}>
         @if ($icon) <x-icon :name="$icon" class="shrink-0 text-gray-500 text-sm w-4"/> @endif
-        @if ($label) {!! __($label) !!} @endif
+        @if ($label) {!! tr($label) !!} @endif
     </a>
 @else
     <div {{ $attributes->class([
         'py-2 px-4 flex items-center gap-3 text-gray-800 cursor-pointer hover:bg-slate-100'
     ])->except(['href', 'icon', 'label']) }}>
         @if ($icon) <x-icon :name="$icon" class="shrink-0 text-gray-500 text-sm w-4"/> @endif
-        @if ($label) {!! __($label) !!} @endif
+        @if ($label) {!! tr($label) !!} @endif
     </div>
 @endif
