f76948ff27592b9904aa4dba5db3c140c8139c71
TJ <tj@jiannius.com>
2023-12-13 07:43:40 +0800

    xmodelable for phone

--- resources/views/components/form/phone.blade.php
+++ resources/views/components/form/phone.blade.php
@@ -1,110 +1,105 @@
+@php
+    $countries = countries()->map(fn($val) => [
+        'flag' => data_get($val, 'flag'),
+        'code' => data_get($val, 'dial_code'),
+        'name' => data_get($val, 'name'),
+    ])->toArray();
+@endphp
+
 <x-form.field {{ $attributes }}>
     <div
         x-data="{
-            show: false,
-            text: null,
-            code: '+60',
+            value: @entangle($attributes->wire('model')),
             focus: false,
-            tel: null,
-            value: @js($attributes->get('value')) || @entangle($attributes->wire('model')),
-            countries: @js(countries()),
+            search: null,
+            dropdown: false,
+            countries: @js($countries),
+            inputs: {
+                code: '+60',
+                tel: null,
+            },
             get flag () {
-                return this.countries.find(cn => (cn.dial_code === this.code))?.flag
+                return this.countries.find(val => (val.code === this.inputs.code))?.flag
             },
             get options () {
-                return this.countries.filter(cn => (
-                    !this.text || cn.name.toLowerCase().includes(this.text.toLowerCase())
-                ))
-            },
-            decode () {
-                const country = this.countries.find(cn => (this.value?.startsWith(cn.dial_code)))
-                if (country) {
-                    this.code = country.dial_code
-                    this.tel = this.value.replace(this.code, '').replace('+', '')
-                }
-            },
-            select (code) {
-                this.code = code
-                this.input()
-                this.close()
-            },
-            open () {
-                this.show = true
-                this.$nextTick(() => {
-                    floatDropdown(this.$refs.anchor, this.$refs.dd)
-                    this.$refs.search.focus()
+                if (!this.search) return this.countries
+
+                return this.countries.filter(val => {
+                    const haystack = `${val.code} ${val.name}`
+                    return haystack.toLowerCase().includes(this.search.toLowerCase())
                 })
             },
-            close () {
-                this.show = false
-                this.text = null
+            open () {
+                this.dropdown = true
+                this.$nextTick(() => this.$refs.search.querySelector('input').focus())
             },
-            input () {
-                this.tel = this.tel ? this.tel.replace(/\D/g, '') : null
-                this.value = this.tel ? `${this.code}${this.tel}` : null
+            select (code) {
+                this.inputs.code = code
+                this.dropdown = false
+                this.search = false
             },
         }"
-        x-init="decode()"
-        x-on:click.away="close()"
-        class="relative"
-    >
-        <div 
-            x-ref="anchor"
-            x-bind:class="{ 'active': focus || show }" 
-            class="form-input flex items-center gap-6 {{
-                component_error(optional($errors), $attributes) ? 'error' : ''
-            }}"
-        >
+        x-init="() => {
+            if (value) {
+                const country = countries.find(val => (value.startsWith(val.code)))
+                inputs.code = country?.code
+                inputs.tel = value.replace(inputs.code, '').replace('+', '')
+            }
+
+            $watch('inputs', () => {
+                if (inputs.code && inputs.tel) value = `${inputs.code}${inputs.tel}`
+                else value = null
+            })
+        }"
+        x-modelable="value"
+        x-on:click.stop="focus = true"
+        x-on:click.away="focus = dropdown = false"
+        class="relative">
+        <div x-ref="anchor" x-bind:class="{ 'active': focus }" class="form-input flex items-center gap-3">
             <a x-on:click="open()" class="flex items-center gap-2">
-                <img x-show="flag" x-bind:src="flag" class="w-4">
-                <div class="text-gray-500" x-text="code"></div>
+                <div class="w-4 h-4 flex">
+                    <template x-if="flag">
+                        <img x-bind:src="flag" class="w-full h-full object-contain">
+                    </template>                    
+                </div>
+                <div class="text-gray-500" x-text="inputs.code"></div>
+                <x-icon name="dropdown-caret"/>
             </a>
 
             <input 
                 type="tel" 
                 x-ref="tel"
-                x-model="tel"
-                x-on:focus="focus = true"
-                x-on:blur="focus = false"
-                x-on:input="input"
-                class="w-full appearance-none border-0 p-0 focus:ring-0"
-            >
+                x-model="inputs.tel"
+                class="w-full appearance-none border-0 p-0 focus:ring-0">
         </div>
 
         <div 
-            x-ref="dd"
-            x-show="show" 
+            x-ref="dropdown"
+            x-show="dropdown" 
             x-transition.opacity
-            class="absolute z-20 w-full bg-white border border-gray-300 shadow-lg rounded-md grid divide-y overflow-hidden"
-        >
-            <div class="py-2 px-4 bg-gray-100 flex items-center gap-2">
-                <x-icon name="search" size="16px" class="text-gray-400"/>
-                <div class="grow">
-                    <input 
-                        type="text" 
-                        x-ref="search"
-                        x-model="text" 
-                        x-on:keydown.enter.prevent="select(options[0]?.dial_code)"
-                        class="bg-transparent appearance-none border-0 p-0 focus:ring-0 w-full"
-                        placeholder="{{ __('Search') }}"
-                    >
-                </div>
+            class="absolute z-10 top-full w-max bg-white border shadow mt-1 rounded-md overflow-hidden">
+            <div x-ref="search" class="p-3 border-b">
+                <x-form.text icon="search"
+                    x-model="search" 
+                    x-on:keydown.enter.prevent="options.length && select(options[0].code)"
+                    placeholder="app.label.search"/>
             </div>
 
-            <div class="h-[250px] overflow-auto">
-                <div class="grid divide-y">
+            <div class="max-h-[250px] overflow-auto">
+                <div class="flex flex-col divide-y">
                     <template x-for="opt in options">
-                        <div 
-                            x-on:click="select(opt.dial_code)"
-                            class="flex items-center gap-2 py-2 px-4 hover:bg-gray-100 cursor-pointer"
-                        >
-                            <img x-show="opt.flag" x-bind:src="opt.flag" class="w-4">
-                            <div x-text="opt.dial_code" class="text-gray-500 shrink-0"></div>
-                            <div x-text="opt.name" class="font-medium text-gray-800"></div>
+                        <div x-on:click="select(opt.code)" class="flex items-center gap-2 py-2 px-4 hover:bg-gray-100 cursor-pointer">
+                            <div class="shrink-0 w-4 h-4 flex">
+                                <template x-if="opt.flag">
+                                    <img x-bind:src="opt.flag" class="w-full h-full object-contain">
+                                </template>                    
+                            </div>
+                            <div class="shrink-0 text-gray-500" x-text="opt.code"></div>
+                            <div class="grow font-medium" x-text="opt.name"></div>
                         </div>
                     </template>
                 </div>
             </div>
-        </div>        
+        </div>
     </div>
 </x-form.field>
