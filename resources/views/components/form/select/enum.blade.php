311723d7effa58edbc3a528fbbd0d3cd18588330
TJ <tj@jiannius.com>
2024-04-12 12:14:24 +0800

    updated listing

--- resources/views/components/form/select/enum.blade.php
+++ resources/views/components/form/select/enum.blade.php
@@ -1,10 +1,9 @@
 @php
     $name = $attributes->get('enum');
     $exclude = $attributes->get('exclude');
+    $options = enum($name)->all();
 
-    $options = $exclude
-        ? enum($name)->all(false)->reject(fn($case) => in_array($$case->name, (array) $exclude))->values()
-        : enum($name)->all();
+    if ($exclude) $options = $options->reject(fn($case) => in_array($case->name, (array) $exclude))->values();
 
     $options = $options->map(fn($case) => $case->option());
 @endphp
