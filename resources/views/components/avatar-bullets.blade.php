d9d920401444958656429aa3d40be8a4cdfd5008
TJ <tj@jiannius.com>
2024-05-15 22:24:47 +0800

    avatar-bullets sizes

--- resources/views/components/avatar-bullets.blade.php
+++ resources/views/components/avatar-bullets.blade.php
@@ -1,5 +1,6 @@
 @php
 $max = $attributes->get('max', 5);
+
 $avatars = collect($attributes->get('avatars'))->map(function($value) {
     $name = get($value, 'name');
     $avatar = get($value, 'avatar');
@@ -8,6 +9,30 @@ $avatars = collect($attributes->get('avatars'))->map(function($value) {
 
     return compact('name', 'avatar');
 });
+
+$size = pick([
+    'xs' => $attributes->has('xs'),
+    'md' => $attributes->has('md'),
+    'lg' => $attributes->has('lg'),
+    'xl' => $attributes->has('xl'),
+    'sm' => true,
+]);
+
+$dimension = [
+    'xs' => '20',
+    'sm' => '24',
+    'md' => '28',
+    'lg' => '32',
+    'xl' => '38',
+][$size];
+
+$text = [
+    'xs' => 'text-xs',
+    'sm' => 'text-sm',
+    'md' => 'text-base',
+    'lg' => 'text-lg',
+    'xl' => 'text-xl',
+][$size];
 @endphp
 
 @if ($avatars->count())
@@ -16,16 +41,16 @@ $avatars = collect($attributes->get('avatars'))->map(function($value) {
         <div 
             x-data
             x-tooltip.raw="{{ get($avatar, 'name') }}"
-            class="-ml-2 first:-ml-0 rounded-full bg-gray-400 text-gray-100 flex items-center justify-center font-semibold border border-gray-300"
-            style="width: 28px; height: 28px; z-index: 1;">
+            class="-ml-2 first:-ml-0 rounded-full bg-gray-200 text-gray-400 flex items-center justify-center font-semibold border border-gray-300 {{ $text }} hover:ring-1 hover:ring-offset-1 hover:ring-gray-300"
+            style="width: {{ $dimension }}px; height: {{ $dimension }}px; z-index: 1;">
             {{ str(get($avatar, 'name'))->substr(0, 1) }}
         </div>
     @endforeach
 
     @if ($avatars->count() > $max)
         <div 
-            class="-ml-2 first:-ml-0 rounded-full bg-gray-400 text-gray-100 text-sm font-medium flex items-center justify-center border border-gray-300"
-            style="width: 28px; height: 28px; z-index: 1;">
+            class="-ml-2 first:-ml-0 rounded-full bg-gray-200 text-gray-400 text-sm font-medium flex items-center justify-center border border-gray-300 {{ $text }}"
+            style="width: {{ $dimension }}px; height: {{ $dimension }}px; z-index: 1;">
             {{ $avatars->count() - $max }}
         </div>
     @endif
