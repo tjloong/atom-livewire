7e36943c14d72608a87baa8eedc885d7b585a22c
TJ <tj@jiannius.com>
2024-05-20 00:41:52 +0800

    delete button delegate callback

--- resources/views/components/button/delete.blade.php
+++ resources/views/components/button/delete.blade.php
@@ -1,34 +1,23 @@
 @php
-    $label = $attributes->get('label') ?? 'common.label.delete';
-    $params = $attributes->get('params');
-    $reload = $attributes->get('reload', false);
-    $inverted = $attributes->get('inverted', true);
-    $callback = $attributes->get('callback', 'delete');
-    $title = $attributes->get('title') ?? 'atom::common.alert.delete.title';
-    $message = $attributes->get('message') ?? 'atom::common.alert.delete.message';
-    
+$label = $attributes->get('label') ?? 'app.label.delete';
+$params = $attributes->get('params');
+$reload = $attributes->get('reload', false);
+$inverted = $attributes->get('inverted', true);
+$callback = $attributes->get('callback', 'delete');
+$title = $attributes->get('title') ?? 'app.alert.delete.title';
+$message = $attributes->get('message') ?? 'app.alert.delete.message';
+$delegate = $attributes->has('x-on:confirm-delete');
+$except = ['c', 'icon', 'label', 'title', 'message', 'callback', 'params', 'reload', 'x-on:click'];
 @endphp
 
-<x-button c="red" icon="trash-can"
-    :label="$label"
-    :inverted="$inverted" 
-    x-on:click="$dispatch('confirm', {
-        title: '{{ __($title) }}',
-        message: '{{ __($message) }}',
-        type: 'error',
-        onConfirmed: () => {
-            $wire.call('{{ $callback }}', {{ json_encode($params) }})
-                .then(() => {{ json_encode($reload) }} && location.reload())
-        },
-    })"
-    {{ $attributes->except([
-        'c', 
-        'icon', 
-        'label', 
-        'title', 
-        'message', 
-        'callback', 
-        'params', 
-        'reload',
-        'x-on:click',
-    ]) }}/>
+<x-button color="red" icon="trash-can" :label="$label" :inverted="$inverted" x-on:click="$dispatch('confirm', {
+    title: '{!!tr($title)!!}',
+    message: '{!!tr($message)!!}',
+    type: 'error',
+    onConfirmed: () => {{Js::from($delegate)}}
+        ? $dispatch('confirm-delete')
+        : $wire.call({{Js::from($callback)}}, {{Js::from($params)}}).then(() => {
+            {{Js::from($reload)}} && location.reload()
+        }),
+})" {{ $attributes->except($except) }}>
+</x-button>
--- resources/views/components/close/delete.blade.php
+++ resources/views/components/close/delete.blade.php
@@ -1,14 +1,17 @@
 @php
     $icon = $attributes->get('icon');
-    $title = $attributes->get('title') ?? 'atom::common.alert.delete.title';
-    $message = $attributes->get('message') ?? 'atom::common.alert.delete.message';
+    $title = $attributes->get('title') ?? 'app.alert.delete.title';
+    $message = $attributes->get('message') ?? 'app.alert.delete.message';
     $callback = $attributes->get('callback', 'delete');
     $params = $attributes->get('params');
+    $delegate = $attributes->has('x-on:confirm-delete');
 @endphp
 
 <x-close color="red" :icon="$icon" x-on:click.stop="$dispatch('confirm', {
-    title: '{{ __($title) }}',
-    message: '{{ __($message) }}',
+    title: '{!!tr($title)!!}',
+    message: '{!!tr($message)!!}',
     type: 'error',
-    onConfirmed: () => $wire.call('{{ $callback }}', {{ json_encode($params) }}),
-})"/>
+    onConfirmed: () => {{Js::from($delegate)}}
+        ? $dispatch('confirm-delete')
+        : $wire.call({{Js::from($callback)}}, {{Js::from($params)}}),
+})" {{ $attributes->only('x-on:confirm-delete') }}/>
