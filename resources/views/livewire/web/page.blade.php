b87c6441d844c7d4fc22f32bf5e56ace6eaca3a8
TJ <tj@jiannius.com>
2024-06-14 16:40:46 +0800

    page layout

--- resources/views/livewire/web/page.blade.php
+++ resources/views/livewire/web/page.blade.php
@@ -1,9 +1,9 @@
-<div class="max-w-screen-xl mx-auto px-4">
+<div class="max-w-screen-lg mx-auto py-20 px-4">
     <div class="text-4xl font-bold mb-6">
         {{ $this->page->title }}
     </div>
 
-    <div class="editor">
+    <div class="editor-content">
         {!! $this->page->content !!}
     </div>
 </div>
