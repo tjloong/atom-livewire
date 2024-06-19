4c993aaaac49ba3a3333c67eb75a9e60e3631f6d
TJ <tj@jiannius.com>
2024-06-19 11:50:16 +0800

    x-tabs size

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -18,15 +18,12 @@ $except = ['tab', 'icon', 'label', 'value', 'count', 'class'];
         'type' => $element === 'button' ? 'button' : null,
         'rel' => $element === 'a' ? $rel : null,
         'target' => $element === 'a' ? $target : null,
-        'class' => 'shrink-0 rounded-md hover:bg-gray-50',
-    ])->except($except) }}>
-    <div
-        x-bind:class="value === {{ Js::from($value) }} ? 'bg-white shadow-sm font-medium whitespace-nowrap w-max' : 'truncate text-gray-400'"
-        class="{{ $attributes->get('class', 'py-1.5 px-3 rounded-md transition-colors duration-300') }}">
-        @if ($slot->isNotEmpty())
-            {{ $slot }}
-        @else
-            {!! tr($label) !!}
-        @endif
-    </div>
+    ])->except($except) }}
+    x-bind:class="value === {{ Js::from($value) }} ? 'bg-white shadow-sm font-medium whitespace-nowrap w-max' : 'truncate text-gray-400'"
+    class="grow transition-colors duration-200 hover:bg-gray-50">
+    @if ($slot->isNotEmpty())
+        {{ $slot }}
+    @else
+        {!! tr($label) !!}
+    @endif
 </{{ $element }}>
--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,15 +1,24 @@
 @php
 $tabs = $attributes->get('tabs', []);
+$size = $attributes->size();
 @endphp
 
-<div x-data="{ value: @entangle($attributes->wire('model')) }" {{ $attributes->merge([
-    'class' => 'relative inline-flex flex-wrap items-center gap-1 select-none w-full p-1 bg-gray-100 rounded-md',
-])->only('class') }}>
-    @if ($slot->isNotEmpty())
-        {{ $slot }}
-    @else
-        @foreach ($tabs as $tab)
-            <x-tab :tab="$tab"/>
-        @endforeach
-    @endif
+<div class="w-full">
+    <div
+        x-data="{ value: @entangle($attributes->wire('model')) }"
+        {{ $attributes->class([
+            'tabs inline-flex items-center gap-1 select-none p-1 bg-gray-100',
+            pick([
+                'rounded *:rounded-sm *:text-sm *:py-1 *:px-3' => $size === 'sm',
+                'rounded-md *:rounded *:py-1.5 *:px-4' => true,
+            ]),
+        ])->only('class') }}>
+        @if ($slot->isNotEmpty())
+            {{ $slot }}
+        @else
+            @foreach ($tabs as $tab)
+                <x-tab :tab="$tab"/>
+            @endforeach
+        @endif
+    </div>
 </div>
