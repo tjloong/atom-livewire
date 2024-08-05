57ed9c66499e2dda86f94bf40118ad971d501c6a
TJ <tj@jiannius.com>
2024-08-05 11:31:41 +0800

    x-toggle

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -4,18 +4,22 @@ $label = $attributes->get('label');
 $caption = $attributes->get('caption');
 @endphp
 
-<div class="group inline-block">
+<div class="group inline-block focus:outline-none">
     <label class="inline-flex gap-3">
         <div class="shrink-0">
             <input type="checkbox" class="hidden peer" {{ $attributes->except('class') }}>
 
             <button
                 type="button"
-                class="w-5 h-5 bg-white rounded-md border border-gray-300 shadow-sm flex items-center justify-center ring-offset-1 focus:outline-none focus:ring-1 focus:ring-theme peer-checked:bg-theme peer-checked:border-theme group-has-[.error]:ring-1 group-has-[.error]:ring-red-500"
+                class="bg-gray-200 rounded-full flex items-center px-px shadow-inner ring-offset-1 focus:outline-none focus:ring-1 focus:ring-theme peer-checked:bg-theme group-has-[.error]:ring-red-500 group-has-[.error]:ring-1"
+                style="width: 38px; height: 20px;"
                 x-on:click.stop="$el.parentNode.querySelector('input').click()">
-                <x-icon name="check" class="text-gray-300 text-xs group-has-[:checked]:text-white"/>
+                <div
+                    class="rounded-full bg-white border shadow transition-transform duration-100 ease-in-out group-has-[:checked]:translate-x-full"
+                    style="width: 18px; height: 18px">
+                </div>
             </button>
-        </div>    
+        </div>
 
         @if ($label)
             <div class="grow space-y-0.5">
