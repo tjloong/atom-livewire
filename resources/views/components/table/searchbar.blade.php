c8625f19ca213202a84df339b32aa0e8a279feca
TJ <tj@jiannius.com>
2024-08-15 12:10:16 +0800

    use short number only if no total provide

--- resources/views/components/table/searchbar.blade.php
+++ resources/views/components/table/searchbar.blade.php
@@ -1,35 +1,34 @@
 @php
-$total = $attributes->get('total') ?? $this->paginator?->total();
-$noSearch = $attributes->get('no-search', false);
-$noMaxRows = $attributes->get('no-max-rows', false);
+$total = $attributes->get('no-total') ? false : $attributes->get('total');
+if (!is_numeric($total)) $total = $this->paginator ? format($this->paginator->total())->short() : false;
 @endphp
 
 <div class="py-3 px-4">
     <div class="flex flex-wrap justify-between items-center gap-2">
         <div class="shrink-0 text-gray-800 flex items-center gap-1.5">
-            @if (is_numeric($total))
+            @if ($total !== false)
                 <div class="text-lg font-medium leading-snug">
-                    {{ format($total)->short() }}
+                    {{ $total }}
                 </div>
 
                 <div class="text-gray-500">
                     {{ tr('app.label.row', $total) }}
                 </div>
-                
-                @if(!$noMaxRows)
-                    <x-button :label="$this->tableMaxRows.' / '.tr('app.label.page')" color="gray" invert xs dropdown="bottom-start">
-                        <div class="flex flex-col divide-y text-sm">
-                            @foreach ([50, 100, 150, 200, 500] as $n)
-                                <x-dropdown.item :label="$n.' / '.tr('app.label.page')" wire:click="$set('tableMaxRows', {{ $n }})"/>
-                            @endforeach
-                        </div>
-                    </x-button>
-                @endisset
+            @endif
+
+            @if(!$attributes->get('no-max-rows'))
+                <x-button :label="$this->tableMaxRows.' / '.tr('app.label.page')" color="gray" invert xs dropdown="bottom-start">
+                    <div class="flex flex-col divide-y text-sm">
+                        @foreach ([50, 100, 150, 200, 500] as $n)
+                            <x-dropdown.item :label="$n.' / '.tr('app.label.page')" wire:click="$set('tableMaxRows', {{ $n }})"/>
+                        @endforeach
+                    </div>
+                </x-button>
             @endif
         </div>
 
         <div class="flex flex-wrap items-center gap-2">
-            @if (!$noSearch)
+            @if (!$attributes->get('no-search'))
                 <div
                     x-data="{
                         text: null,
