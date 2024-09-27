70402cd70adee47b966d95da2b37f3b97ac5718f
TJ <tj@jiannius.com>
2024-09-27 17:42:58 +0800

    Alpine.data(toast)

--- components/toast.blade.php
+++ components/toast.blade.php
@@ -12,49 +12,7 @@ $position = [
 @endphp
 
 <div
-    x-data="{
-        toasts: @js($toasts),
-
-        init () {
-            this.$nextTick(() => {
-                this.toasts = this.toasts.map(toast => (this.build(toast)))
-                this.toasts.forEach(toast => this.show(toast))
-            })
-        },
-
-        build (toast) {
-            return {
-                id: atom.ulid(),
-                visible: false,
-                theme: 'dark',
-                ...toast,
-            }
-        },
-
-        show (toast) {
-            if (toast.visible) return
-            setTimeout(() => toast.visible = true, 100)
-            if (!toast.permanent) setTimeout(() => this.close(toast), toast.delay || 3500)
-        },
-
-        close (toast) {
-            toast.visible = false
-
-            setTimeout(() => {
-                let index = this.toasts.findIndex(item => (item.id === toast.id))
-                this.toasts.splice(index, 1)
-            }, 100)
-        },
-
-        push (value) {
-            if (Array.isArray(value)) value.forEach(item => this.push(item))
-            else {
-                if (this.toasts.length >= 4) this.toasts.shift()
-                this.toasts.push(this.build(value))
-                this.toasts.forEach(toast => this.show(toast))
-            }
-        },
-    }"
+    x-data="toast(@js($toasts))"
     x-on:toast-received.window="push($event.detail)"
     data-atom-toast>
     <template x-teleport="body">
