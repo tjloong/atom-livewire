99151ef6bc1418e1f0dcee5cd9fe275681b5b6aa
TJ <tj@jiannius.com>
2024-11-14 17:06:16 +0800

    label actions

--- components/_label.blade.php
+++ components/_label.blade.php
@@ -17,10 +17,33 @@ $attrs = $attributes
     ;
 @endphp
 
-<label {{ $attrs }} data-atom-label>
-    @if ($icon)
-        <atom:icon :name="$icon" size="15" class="shrink-0"/>
-    @endif
+@isset ($actions)
+    <div class="group flex items-center gap-2 justify-between">
+        <atom:_label :attributes="$attributes">
+            {{ $slot }}
+        </atom:_label>
 
-    {{ $slot }}
-</label>
+        <div class="shrink-0 hidden group-hover:block">
+            @if ($actions->isEmpty())
+                <div
+                    @if ($actions->attributes->has('tooltip'))
+                    x-tooltip="{{ js(t($actions->attributes->get('tooltip'))) }}"
+                    @endif
+                    class="w-5 h-5 flex items-center justify-center text-muted-more cursor-pointer"
+                    {{ $actions->attributes->except(['tooltip', 'icon']) }}>
+                    <atom:icon :name="$actions->attributes->get('icon')" size="13"/>
+                </div>
+            @else
+                {{ $actions }}
+            @endif
+        </div>
+    </div>
+@else
+    <label {{ $attrs }} data-atom-label>
+        @if ($icon)
+            <atom:icon :name="$icon" size="15" class="shrink-0"/>
+        @endif
+
+        {{ $slot }}
+    </label>
+@endisset
