8f6ea22b60efbc6fc7c15b5d584c99c63cdb69a6
TJ <tj@jiannius.com>
2025-02-22 01:23:11 +0800

    minor

--- components/breadcrumb.blade.php
+++ components/breadcrumb.blade.php
@@ -17,20 +17,20 @@ $attrs = $attributes->except(['traces']);
     </div>
 @elseif ($traces->count() > 1)
     <ol
-        itemscope
-        itemtype="https://schema.org/BreadcrumbList"
-        {{ $attrs->class(['flex flex-wrap items-center gap-2 py-3 px-6 overflow-hidden']) }}>
+    itemscope
+    itemtype="https://schema.org/BreadcrumbList"
+    {{ $attrs->class(['flex flex-wrap items-center gap-2 py-3 px-6 overflow-hidden']) }}>
         @foreach ($traces as $i => $trace)
             <li
-                itemscope 
-                itemprop="itemListElement" 
-                itemtype="https://schema.org/ListItem"
-                class="shrink-0 max-w-40 lg:max-w-64">
+            itemscope 
+            itemprop="itemListElement" 
+            itemtype="https://schema.org/ListItem"
+            class="shrink-0 max-w-40 lg:max-w-64">
                 <div class="flex items-center gap-2">
                     @if ($trace->icon)
-                        <x-icon :name="$trace->icon" class="text-zinc-400 shrink-0"/>
+                        <atom:icon :name="$trace->icon" class="text-zinc-400 shrink-0"/>
                     @elseif ($i === 0)
-                        <x-icon home class="text-zinc-400 shrink-0"/>
+                        <atom:icon home class="text-zinc-400 shrink-0"/>
                     @endif
 
                     @if ($trace->href && $i !== $traces->keys()->last())
@@ -44,7 +44,7 @@ $attrs = $attributes->except(['traces']);
                     @endif
 
                     @if ($i !== $traces->keys()->last())
-                        <x-icon right class="shrink-0 text-zinc-400"/>
+                        <atom:icon right class="shrink-0 text-zinc-400"/>
                     @endif
                 </div>
 
@@ -54,25 +54,24 @@ $attrs = $attributes->except(['traces']);
     </ol>
 @else
     <ol
-        wire:ignore
-        x-data="breadcrumb()"
-        x-show="traces.length > 1"
-        x-on:sheet-changed.window="build()"
-        data-atom-breadcrumb
-        {{ $attrs->class(['flex flex-wrap items-center gap-2 py-3 overflow-hidden first:mb-4']) }}>
+    wire:ignore
+    x-data="breadcrumb()"
+    x-show="traces.length > 1"
+    x-on:sheet-changed.window="build()"
+    data-atom-breadcrumb
+    {{ $attrs->class(['flex flex-wrap items-center gap-2 py-3 overflow-hidden first:mb-4']) }}>
         <template x-for="(trace, i) in traces" hidden>
             <li class="shrink-0 max-w-40 lg:max-w-64">
                 <div class="flex items-center gap-2 truncate">
-                    <x-icon home x-show="i === 0" class="text-zinc-400 shrink-0"/>
+                    <atom:icon home x-show="i === 0" class="text-zinc-400 shrink-0"/>
 
                     <template x-if="i !== traces.lastIndex()" hidden>
                         <div class="flex items-center gap-2 truncate">
                             <a
-                                x-text="trace.label"
-                                x-on:click="Atom.sheet(trace.name).show()"
-                                class="text-zinc-800 font-medium truncate whitespace-nowrap">
-                            </a>
-                            <x-icon right class="shrink-0 text-zinc-400"/>
+                            x-text="trace.label"
+                            x-on:click="Atom.sheet(trace.name).show()"
+                            class="text-zinc-800 font-medium truncate whitespace-nowrap"></a>
+                            <atom:icon right class="shrink-0 text-zinc-400"/>
                         </div>
                     </template>
 
