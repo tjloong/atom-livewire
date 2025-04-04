130687930e9e6ba2c49da750bc7e90684936d039
TJ <tj@jiannius.com>
2025-04-04 18:07:51 +0800

    fix: whatsapp button zindex

--- components/whatsapp.blade.php
+++ components/whatsapp.blade.php
@@ -10,6 +10,7 @@ $url = collect([
 <a
 href="{{ $url }}"
 target="_blank"
-class="fixed z-90 right-14 bottom-14 bg-green-500 rounded-full shadow flex items-center justify-center gap-3 py-2 px-5 text-lg text-white transition-transform hover:scale-110">
+style="z-index: 200;"
+class="fixed right-14 bottom-14 bg-green-500 rounded-full shadow flex items-center justify-center gap-3 py-2 px-5 text-lg text-white transition-transform hover:scale-110">
     <atom:icon whatsapp size="28"/> @t('whatsapp-us')
 </a>
